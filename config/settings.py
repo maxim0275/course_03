85cdc59ea77cf9bc17298848568252a42311bf70
maxim0275 <maxim0275@yandex.ru>
2026-01-31 18:26:02 +0500

    c_06

--- config/settings.py
+++ config/settings.py
@@ -1,27 +1,18 @@
-
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ['*']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -66,24 +57,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,7 +84,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -111,7 +94,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
--- mailings/management/commands/send_message.py
+++ mailings/management/commands/send_message.py
@@ -1,14 +1,14 @@
-from django.utils import timezone
-from config.settings import EMAIL_HOST_USER
-from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from config.settings import EMAIL_HOST_USER
 from mailings.models import Mailing, MailingAttempt
 
 
 def send_mailing(mailing):
     recipients = mailing.recipients.all()
     successful_attempts = 0
-    total_recipients = recipients.count()
 
     for recipient in recipients:
         try:
@@ -26,7 +26,7 @@ def send_mailing(mailing):
             )
             successful_attempts += 1
             print(
-                f'Сообщение {mailing.message.subject} успешно отправлено на почту {recipient.email}'
+                f'Сообщение {mailing.message.subject} успешно отправлено {recipient.email}'
             )
         except Exception as e:
             MailingAttempt.objects.create(
@@ -43,6 +43,7 @@ def send_mailing(mailing):
             mailing.first_dispatch = timezone.now()
 
     mailing.save()
+
 
 class Command(BaseCommand):
     help = 'Send messages'
